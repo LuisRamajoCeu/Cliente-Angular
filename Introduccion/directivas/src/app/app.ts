ab5269d64e3eeb9bf6f5e25f800033265cdcfadf
LuisRamajoCeu <lramajo2587@ceuandalucia.es>
2025-11-25 19:27:31 +0100

    mensajes

--- Introduccion/directivas/src/app/app.ts
+++ Introduccion/directivas/src/app/app.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('directivas');
+  title:string = 'Alta de usuario';
+  morado:string = "morado";
 }
