f625b9f7d3544391fa27e24a13985541decaafa2
LuisRamajoCeu <lramajo2587@ceuandalucia.es>
2025-11-28 15:55:43 +0100

    .

--- Introduccion/directivas/src/app/articulo/articulo.ts
+++ Introduccion/directivas/src/app/articulo/articulo.ts
@@ -1,35 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ArticuloModel } from '../model/articuloModel';
-
+import { CommonModule } from '@angular/common';
+ 
 @Component({
   selector: 'app-articulo',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './articulo.html',
   styleUrl: './articulo.css',
 })
 export class Articulo {
-  nombre:string = '';
-  precio:number = 0;
-  unidades:number = 0;
-  articulos: ArticuloModel[] = [
-    {
-      nombre: 'Teclado',
-      precio: 75.50,
-      unidades: 15
-    },
-    {
-      nombre: 'Monitor',
-      precio: 219.99,
-      unidades: 7
-    },
-    {
-      nombre: 'Ratón',
-      precio: 25.00,
-      unidades: 30
-    }];
-    registrar(event:any){
-      this.nombre = event.target.innerHTML;
-      this.precio = event.target.innerHTML;
-      this.unidades = event.target.innerHTML - 1;
-    }
+  nombre: string = '';
+  precio: number = 0;
+  unidades: number = 0;
+ 
+  art1 = { nombre: 'Papa', precio: 10.1, unidades: 30 };
+  art2 = { nombre: 'Nosilla', precio: 4, unidades: 50 };
+  art3 = { nombre: 'Xocos', precio: 2.50, unidades: 50 };
+  articulos: ArticuloModel[] = [this.art1, this.art2, this.art3];
+  comprar() {
+  this.articulos.push({
+    nombre: this.nombre,
+    precio: this.precio,
+    unidades: this.unidades,
+  });
+  }
 }
